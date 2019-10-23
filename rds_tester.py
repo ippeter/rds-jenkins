0d06eddd3a8fca7f8c906e4161c560b676a24083
Peter Predtechensky <Petr.Predtechensky@gmail.com>
2019-10-23 10:39:54 +0300

    Delete app.config

--- rds_tester.py
+++ rds_tester.py
@@ -12,8 +12,6 @@ class ReusableForm(Form):
 
 # Instantiate our Node
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
